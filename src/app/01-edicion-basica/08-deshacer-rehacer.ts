ff7ed8a9baf3f75d5e4e189e1cc316e561f9f0c6
Jose Montenegro <grafijoss@hotmail.com>
2022-10-13 20:34:09 -0500

    first commit

---
+++ src/app/01-edicion-basica/08-deshacer-rehacer.ts
@@ -0,0 +1,24 @@
+/*
+    Objetivo:
+        Deshacer y rehacer el código
+§
+    Tips:
+        ⌘ Z
+        ⌘ ⇧ Z
+        Ctrl +Z
+        Ctrl + Shift + Z
+        Ctrl + y
+
+*/
+
+
+
+
+
+
+// Demo
+// function holaMundo() {
+//     return 'Saludos a todos!';
+// }
+
+
